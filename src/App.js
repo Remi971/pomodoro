35b174a41dc4549e0cd5f2bd2e3fc503104ef427
Remi <remi.bha@gmail.com>
2021-02-09 20:41:45 +0100

    24/29 and failed transition background linear-gradient

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     const timer = setInterval(() => {
       if (time > 0 && operate) {
         setTime((t) => t - 1);
-        console.log(time);
+        console.log(time, state, display);
       }
     },1000);
     check();
@@ -26,10 +26,11 @@ function App() {
 )
 
   const check = () => {
+    let sound = document.getElementById('beep');
     if (time === 0) {
-      setTimeout(() => {
+      sound.currentTime = 0;
         if (state) {
-          setState(false);
+          setState((s) => false);
           setTime(breakTime * 60);
           setOperate(true);
         }else {
@@ -37,7 +38,7 @@ function App() {
           setTime(sessionTime * 60);
           setOperate(true);
         }
-      }, 1000)
+        sound.play();
     }
   }
 
@@ -99,6 +100,9 @@ function App() {
   }
 
   const reset = () => {
+    let sound = document.getElementById('beep');
+    sound.pause();
+    sound.currentTime = 0;
     setOperate(false);
     const content = document.getElementById('start_stop');
     const wrapper = document.getElementById('wrapper');
@@ -126,15 +130,23 @@ function App() {
   }
 
   const settingTime = (e) => {
-    if (e.target.id === "break-increment" && breakTime < 60) {
-      setBreakTime(breakTime + 1);
-      if (document.querySelector('body').classList.value === 'on') {
-        setTime((breakTime+1) * 60);
+    if (e.target.id === "break-increment") {
+      if (breakTime < 59){
+        setBreakTime(breakTime + 1);
+        if (document.querySelector('body').classList.value === 'on') {
+          setTime((breakTime+1) * 60);
+        } else {
+          console.log("Break time is too high...")
+        }
       }
-    } else if (e.target.id === "session-increment" && sessionTime < 60) {
-      setSessionTime(sessionTime + 1);
-      if (document.querySelector('body').classList.value !== 'on') {
-        setTime((sessionTime+1) * 60);
+    } else if (e.target.id === "session-increment") {
+      if (sessionTime < 59) {
+        setSessionTime(sessionTime + 1);
+        if (document.querySelector('body').classList.value !== 'on') {
+          setTime((sessionTime+1) * 60);
+        } else {
+          console.log("Session time is too high...")
+        }
       }
     } else if (e.target.id === "break-decrement") {
       if (breakTime > 1) {
@@ -182,16 +194,20 @@ const timeLeft = () => {
         </div>
         <h3 id='timer-label'>{display}</h3>
         <h1 id='time-left'>{timeLeft()}</h1>
+        <audio
+          id='beep'
+          src="https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"
+        />
         <div className='bottom_btn'>
           <Btn
             id='start_stop'
-            className='start_stop specific'
+            className='specific'
             func={startStop}
             content='Start'
           />
           <Btn
             id='reset'
-            className='reset specific'
+            className='specific'
             func={reset}
             content='Reset'
           />
